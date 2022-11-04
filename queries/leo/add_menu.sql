b9bbfcd0bda5a8bcea12a45d870a94ef15caaf95
nickj <consultor.nicholas@leomadeiras.com.br>
2022-11-04 10:53:42 -0300

    updated lint and added table to add_Menu

--- queries/leo/add_menu.sql
+++ queries/leo/add_menu.sql
@@ -1,3 +1,5 @@
+use [fl_db-fl4qa]
+
 insert into menu (Descricao,Action,Controller,UrlExterna,Ordem,IdPai,ClassHtml,IdHtml,IdTipo,IdClass)
 values('Admin', NULL, NULL, NULL, 16, 15, NULL, NULL, 4, 'admin');
 
